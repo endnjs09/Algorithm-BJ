5c18e72d8166bef149f3ad7fd211ef64534e4ba0
het0 <59733604+endnjs09@users.noreply.github.com>
2026-02-09 22:49:21 +0900

    [Gold V] Title: 두 용액, Time: 12 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Gold/2470. 두 용액/두 용액.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#define INF 2e9 + 1
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+
+    int N;      // 용액의 수
+    cin >> N;
+    vector<int> arr(N);
+
+    for (int i = 0; i < N; i++) cin >> arr[i];
+    sort(arr.begin(), arr.end());
+
+    // -99, -2, -1, 4, 98
+
+    int start = 0, end = arr.size() - 1, tmp = INF;
+    int L, R;
+    while (start < end) {
+        long long sum = (long long)arr[start] + arr[end];
+
+        if (abs(sum) < tmp) {   
+            tmp = abs(sum);      // 갱신용
+            L = start;      
+            R = end;
+        }
+
+        if (sum < 0) start++;
+        else if (sum > 0) end--;
+        else break;
+    }
+
+    cout << arr[L] << " " << arr[R];
+}
