bf51f854933ba0c91372ce9d783a2c90079ad6ce
het0 <59733604+endnjs09@users.noreply.github.com>
2026-02-09 22:48:57 +0900

    [Gold V] Title: 회의실 배정, Time: 24 ms, Memory: 2808 KB -BaekjoonHub

---
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+bool sorts(pair<int, int> a, pair<int, int> b) {
+	if (a.second == b.second)
+		return a.first < b.first;
+	return a.second < b.second;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr); cout.tie(nullptr);
+
+	int num, end = 0, count = 0;
+	cin >> num;
+	vector<pair<int, int>> v(num);
+	for (int i = 0; i < num; i++) 
+		cin >> v[i].first >> v[i].second;
+	sort(v.begin(), v.end(), sorts);
+
+	for (int i = 0; i < num; i++) {
+		if (v[i].first >= end) {
+			end = v[i].second;
+			count++;
+		}
+	}
+
+	cout << count;
+}
+
