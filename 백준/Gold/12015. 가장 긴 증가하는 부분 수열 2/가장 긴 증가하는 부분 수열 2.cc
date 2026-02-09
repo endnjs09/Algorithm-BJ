3057382bbd5bafab181daea2f4983b74a6e66e5e
het0 <59733604+endnjs09@users.noreply.github.com>
2026-02-09 22:31:38 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 112 ms, Memory: 12200 KB -BaekjoonHub

---
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    
+    int N;
+    cin >> N;
+    vector<int> v(N);
+    vector<int> tmp;
+    for (int i = 0; i < N; i++) {
+        cin >> v[i];
+    }
+
+    for (int num : v) {
+
+        if (tmp.empty() || num > tmp.back()) {  // 마지막 값보다 크면 넣음
+            tmp.push_back(num);
+        }
+        else {  // 마지막 값 보다 작으면 들어갈 자리 찾음
+            int t = lower_bound(tmp.begin(), tmp.end(), num) - tmp.begin(); // num 값보다 크거나 같은 위치
+            tmp[t] = num;
+        }
+    }     
+
+    cout << tmp.size();
+}
