2eaf6a9059dc51befa62058637db96075c970305
endnjs <59733604+endnjs09@users.noreply.github.com>
2026-03-27 22:22:13 +0900

    [Platinum III] Title: 도시 왕복하기 2, Time: 12 ms, Memory: 7456 KB -BaekjoonHub

---
+++ 백준/Platinum/2316. 도시 왕복하기 2/도시 왕복하기 2.cc
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <string>
+using namespace std;
+#define SIZE 801
+
+const int INF = 1e9;
+
+// capacity, flow
+int cp[SIZE][SIZE], fw[SIZE][SIZE];
+int parent[SIZE];   
+vector<int> adj[SIZE];
+
+int flow(int start, int end) {
+    int total = 0;
+
+    while (1) {
+        fill(parent, parent + SIZE, -1);
+        queue<int> q;
+        q.push(start);
+
+        while (!q.empty()) {
+            int current = q.front();
+            q.pop();
+
+            for (int next : adj[current]) {
+                // 잔여 용량 남았고 방문 안했으면
+                if (cp[current][next] > fw[current][next] && parent[next] == -1) {
+                    q.push(next);
+                    parent[next] = current;
+                    if (next == end) break;
+                }
+            }
+        }
+        // 더이상 경로가 없으면
+        if (parent[end] == -1) break;
+
+        int i = end;    // 도착지(2)에서 출발
+        while (i != start) {    // 출발지에 도착할 때까지 역이동
+            int p = parent[i];
+            fw[p][i] += 1;  // 이동한 길
+            fw[i][p] -= 1;  // 반대
+
+            i = p;  
+        }
+        total++;
+    }
+
+    return total;
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int N, P;
+    cin >> N >> P;
+
+    for (int i = 1; i <= N; i++) {
+        int in = i;         // 들어오는 길 (1~..)
+        int out = i + 400;  // 나가는 길 (401~..)
+        adj[in].push_back(out);
+        adj[out].push_back(in);
+
+        cp[in][out] = 1;
+    }
+
+    for (int i = 0; i < P; i++) {
+        int u, v;
+        cin >> u >> v;
+
+        adj[u + 400].push_back(v);
+        adj[v].push_back(u + 400);
+        cp[u + 400][v] = 1;
+
+        adj[v + 400].push_back(u);
+        adj[u].push_back(v + 400);
+        cp[v + 400][u] = 1;
+    }
+
+    cout << flow(401, 2);
+}
